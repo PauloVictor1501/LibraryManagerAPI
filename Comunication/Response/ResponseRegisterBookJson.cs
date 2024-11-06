fc44409e2c25783bb9ed7471f7456a007550703c
PauloVictor1501 <paulovictor1501@gmail.com>
2024-11-05 21:59:19 -0300

    Adicionar arquivos de projeto.

---
+++ Comunication/Response/ResponseRegisterBookJson.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryManagerAPI.Comunication.Response;
+
+public class ResponseRegisterBookJson
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using LibraryManagerAPI.Comunication.Request;
+using LibraryManagerAPI.Comunication.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagerAPI.Controllers;
+
+public class BookController : ControllerBase
+{
+    [Route("api/[controller]")]
+    [HttpPost]
+    [ProducesResponseType(typeof(RequestRegisterBookJson), StatusCodes.Status201Created)]
+
+    public IActionResult Create([FromBody] RequestRegisterBookJson request)
+    {
+        
+        var response = new ResponseRegisterBookJson
+        {
+            Id = request.Id,
+            Title = request.Title
+        };
+
+        if (request.Id == 0)
+        {
+            return BadRequest("O campo 'Id' é obrigatório.");
+        }
+
+        return Created(string.Empty, response);
+    }
+    
+}
