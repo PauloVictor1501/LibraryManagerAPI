0383fee74b3e2539dc5bff8833bf249171e56e41
PauloVictor1501 <paulovictor1501@gmail.com>
2024-11-10 23:12:32 -0300

    Inserido métodos get, put e delete

---
+++ Comunication/Request/RequestUpdateBookJson.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagerAPI.Comunication.Request;
+
+public class RequestUpdateBookJson
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public float Price { get; set; }
+}
--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -4,15 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagerAPI.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class BookController : ControllerBase
 {
-    [Route("api/[controller]")]
+    private static List<Book> _books = new List<Book> { };
+
     [HttpPost]
     [ProducesResponseType(typeof(RequestRegisterBookJson), StatusCodes.Status201Created)]
 
     public IActionResult Create([FromBody] RequestRegisterBookJson request)
     {
-        
+
         var response = new ResponseRegisterBookJson
         {
             Id = request.Id,
@@ -24,7 +27,82 @@ public class BookController : ControllerBase
             return BadRequest("O campo 'Id' é obrigatório.");
         }
 
+        _books.Add(new Book { Id = request.Id, Title = request.Title });
+
         return Created(string.Empty, response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+    public IActionResult GetBook()
+    {
+        return Ok(_books);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
+    public IActionResult Update([FromRoute] int id, [FromBody] RequestUpdateBookJson request)
+    {
+        Book? FindBookById(int id)
+        {
+            foreach (var book in _books)
+            {
+                if (book.Id == id)
+                {
+                    return book;
+                }
+            }
+
+            return null;
+        }
+        var book = FindBookById(id);
+
+        if (book == null)
+        {
+            return NotFound("Livro não encontrado.");
+        }
+
+        book.Title = request.Title;
+        book.Author = request.Author;
+        book.Price = request.Price;
+        book.Genre = request.Genre;
+
+        return Ok(book);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+    public IActionResult Delete(int id)
+    {
+        Book? FindBookById(int id)
+        {
+            foreach (var book in _books)
+            {
+                if (book.Id == id)
+                {
+                    return book;
+                }
+            }
+
+            return null;
+        }
+        var book = FindBookById(id);
+
+        if (book == null)
+        {
+            return NotFound("Livro não encontrado.");
+        }
+
+        _books.Remove(book);
+
+        return Ok($"Livro com Id {id} deletado com sucesso");
     }
     
 }
